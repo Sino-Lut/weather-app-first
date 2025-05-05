b1cc6acb08747a64528b53b1c9bd6eca22c4ae7b
Sino-Lut <slutya@gmail.com>
2025-05-05 16:07:16 +0200

    Added the search engine functionality using API

--- first-weather-app/src/index.js
+++ first-weather-app/src/index.js
@@ -1,1 +1,23 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
 
+  searchCity(searchInput.value);
+}
+
+function searchCity(city) {
+  let apiKey = "96t4883dea40738f8c4oe83b37b0ab90";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", search);
