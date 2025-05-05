73207d287c56d1015fe79cff47d5b39b4d930287
Sino-Lut <slutya@gmail.com>
2025-05-05 21:47:08 +0200

    Added weather data in JS

--- first-weather-app/src/index.js
+++ first-weather-app/src/index.js
@@ -12,11 +12,43 @@ function searchCity(city) {
 }
 
 function displayWeather(response) {
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+
+  timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class = "weather-icon"/>`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 let searchFormElement = document.querySelector("#search-form");
